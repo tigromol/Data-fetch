0e240abbd4f2d2a1d7bf7035bc23e425cfda62a7
Misgorod <andbulgaru@gmail.com>
2019-02-20 22:56:04 +0300

    Import parser

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from argparse import ArgumentParser
 import urllib.request
+import sys
+
 query = {
     'market': "1", 
     'em': "175924",
@@ -31,10 +34,14 @@ query = {
 def parse(file_path):
     pass
 
-def main():
+def parse_args(argv):
+    pass
+
+def main(argv):
+    parse_args(argv)
     req = urllib.request.Request(url=f"http://export.finam.ru/POLY_170620_170623.txt?market={query['market']}&em={query['em']}&code={query['code']}&apply={query['apply']}&df={query['df']}&mf={query['mf']}&yf={query['yf']}&from={query['from']}&dt={query['dt']}&mt={query['mt']}&yt={query['yt']}&to={query['to']}&p={query['p']}&f={query['f']}&e={query['e']}&cn={query['cn']}&dtf={query['dtf']}&tmf={query['tmf']}&MSOR={query['MSOR']}&mstime={query['mstime']}&mstimever={query['mstimever']}&sep={query['sep']}&sep2={query['sep2']}&datf={query['datf']}&at={query['at']}")
     with urllib.request.urlopen(req) as readFile, open("test.txt", mode="w", encoding="utf-8") as writeFile:
         writeFile.write(readFile.read().decode('utf-8'))
+
 if __name__ == "__main__":
-    print("start")
-    main()
+    main(sys.argv[1:])
