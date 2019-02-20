593f7bf0059572aa180f0e0a15dcbf30d2fc7b8f
Misgorod <andbulgaru@gmail.com>
2019-02-21 01:53:28 +0300

    Parsed templates to dicts

--- datafetch.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-from iexfinance.stocks import get_historical_intraday
-
-date = datetime(2010, 11, 27)
-
-print(len(get_historical_intraday("DJD", date)))
--- parse/codes_parser.py
+++ parse/codes_parser.py
@@ -1,6 +1,9 @@
+import os
 from bs4 import BeautifulSoup
+dir = os.path.dirname(__file__)
 
-with open("codes.html") as fp: # html open
+filename = os.path.join(dir, '../exchanges/ex520.html')
+with open(filename, encoding="utf-8") as fp: # html open
     soup = BeautifulSoup(fp)
 array = soup.find_all('a') #list of <a>
 arraycont = []
@@ -11,7 +14,9 @@ for x in array:
     arrayval.append(x['value'])
 for x in range(len(arraycont)):
     d[''.join(arraycont[x])] = arrayval[x]
-f1 = open('dict.txt', 'w') #name of file
+
+filename = os.path.join(dir, '../exchanges/ex520.py')
+f1 = open(filename, 'w', encoding="utf-8") #name of file
 
 f1.write(str(d))
 f1.close()
