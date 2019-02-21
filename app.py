f1f97de55fabe9244d6fd97ce63e5fc093c6725e
Misgorod <andbulgaru@gmail.com>
2019-02-21 23:25:45 +0300

    Add data parse

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from datetime import datetime
+import re
 import os
 import urllib.request
 import sys
 from gen import gen_next_month
 import time
 from exchanges.codes import codes
-
-dir = os.path.dirname(__file__)
 
 query = {
     'market': "1", # Говорит о том, где вращается бумага(инструмент)
@@ -39,10 +38,26 @@ query = {
                  #4 — TICKER, PER, DATE, TIME, CLOSE; #5 — DATE, TIME, OPEN, HIGH, LOW, CLOSE, VOL; #6 — DATE, TIME, LAST, VOL, ID, OPER).
     'at': "1" # добавлять заголовок в файл (0 — нет, 1 — да)
 }
+
+dir = os.path.dirname(__file__)
+data_path = os.path.join(dir, "data")
+
 def parse(file_path):
     pass
 
-# code start_date month_count
+def get_data(dir_path):
+    for file in os.listdir(dir_path):
+        data = []
+        file_name = os.path.join(dir_path, file)
+        with open(file_name, mode="r", encoding="utf-8") as reader:
+            for line in reader:
+                match = re.search(r"^.+,\d+,\d+,\d+,(\d+.\d+)$", line)
+                if match:
+                    data.append(match.group(1))
+        yield data
+
+
+
 def parse_args(argv):
     parser = ArgumentParser(description='parse files via que request')
     parser.add_argument("code", help="Code of the instrument")
@@ -61,7 +76,7 @@ def parse_args(argv):
 
 def make_query():
     req = urllib.request.Request(url=f"http://export.finam.ru/POLY_170620_170623.txt?market={query['market']}&em={query['em']}&code={query['code']}&apply={query['apply']}&df={query['df']}&mf={query['mf']}&yf={query['yf']}&from={query['from']}&dt={query['dt']}&mt={query['mt']}&yt={query['yt']}&to={query['to']}&p={query['p']}&f={query['f']}&e={query['e']}&cn={query['cn']}&dtf={query['dtf']}&tmf={query['tmf']}&MSOR={query['MSOR']}&mstime={query['mstime']}&mstimever={query['mstimever']}&sep={query['sep']}&sep2={query['sep2']}&datf={query['datf']}&at={query['at']}")
-    file_name = os.path.join(dir, "data", f"{query['code']}_{query['from']}_{query['to']}.txt")
+    file_name = os.path.join(data_path, f"{query['code']}_{query['from']}_{query['to']}.txt")
     with urllib.request.urlopen(req) as read_file, open(file_name, mode="w", encoding="utf-8") as write_file:
         try:
             write_file.write(read_file.read().decode('utf-8'))
@@ -79,22 +94,23 @@ def parse_date(start_date, final_date):
     query["yt"] = query['to'].split(".")[2]
 
 def main(argv):
-    dates = parse_args(argv)
-    date_gen = gen_next_month(dates["start"], dates["final"])
-    prev_date = dates["start"]
-    try:
-        while True:
-            next_date = next(date_gen).strftime("%d.%m.%Y")
-            print(prev_date)
-            print(next_date)
-            parse_date(prev_date, next_date)
-            prev_date = next_date
+    for arr in get_data(data_path):
+        print(arr)
+    # dates = parse_args(argv)
+    # date_gen = gen_next_month(dates["start"], dates["final"])
+    # prev_date = dates["start"]
+    # try:
+    #     while True:
+    #         next_date = next(date_gen).strftime("%d.%m.%Y")
+    #         print(prev_date)
+    #         print(next_date)
+    #         parse_date(prev_date, next_date)
+    #         prev_date = next_date
 
-            make_query()
-            time.sleep(1)
-    except StopIteration:
-        print("Success")
-    
+    #         make_query()
+    #         time.sleep(1)
+    # except StopIteration:
+    #     print("Success")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
